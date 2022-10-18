dbaa06a938685095c1475e643de211d32a3b2630
Arthur Zadneprovskiy <hitsuga2000@gmail.com>
2022-10-18 17:47:38 +0200

    fix: store page route

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import ProductPage from "../pages/ProductPage";
 import StorePage from "../pages/StorePage";
 
 export const publickRoutes = [
-  { path: "*", element: <StorePage /> },
+  { path: "/clothes-store", element: <StorePage /> },
   { path: "/product/:title", element: <ProductPage /> },
   // { path: "/order", element: <OrderPage /> },
 ];
